c48d626e220b20360d8298d0703e0d5c346d61da
Fabio Ugo Venchiarutti <fabio@vuole.me>
2025-07-04 11:09:26 +0200

    unwinding function. Need additiona star mark in chunks

--- fabio.c
+++ fabio.c
@@ -8,9 +8,9 @@
 #endif
 
 
-/* An individual inked list item */
+/* An individual inked list item. Each item never contains bytes from multiple lines */
 typedef struct GNLChunk {
-	ssize_t         mark;                   // usable length (either offset of the next \n or len at EOF)
+	ssize_t         len;                    // usable length (either offset of next \n, EOF or BUFFER_SIZE)
 	struct GNLChunk *next;                  // pointer to the next item
 	char            buf[BUFFER_SIZE];       // the buffer itself
 } GNLChunk;
@@ -43,38 +43,6 @@ typedef struct GNLTracker {
 } GNLTracker;
 
 
-/*
-	Unwinds a linked list into dest, backwards in order to be able to
-	bring back the last element at each iteration, and eventually free
-	that one too if there is no data left
-
-	Frees the list elements backwards from the first one containing 
-
-	Stops at the first \n (included) or the end of the last buffer
-
-	If there is remaining data after \n in the last
-
-	NOTE: frees the very pointer in the linked list that is passed to it
-
-	Returns:
-		Total number of bytes written, including the final \n
-*/
-// size_t ll_actions(char *dest, GNLChunk *head) {
-// size_t ret;
-
-// 	ret = 0;
-// 	if (head->next != NULL) {
-// 		// look ahead into the list if this is not the end.
-// 		// Copy
-// 		ret += assemble_and_free(dest + BUFFER_SIZE, head->next, free_only);
-// 	}
-// 	if (NULL != dest)
-// 		ret += (head->buf += gnl_copy(
-// 			dest, head->buf + head->read, BUFFER_SIZE - head->read
-// 		));
-// 	
-// 	free(head);
-
 // 	return ret;
 
 // }
@@ -84,7 +52,8 @@ typedef struct GNLTracker {
 GNLChunk **expand_fd_catalog(GNLTracker *track, int fd) {
 GNLChunk **ret;
 int i;
-
+	if (fd < 0)
+		return NULL;
 	// static initialization sentinels
 	ret = track->latest;
 	track->max = ret ? track->max : -1;
@@ -102,34 +71,84 @@ int i;
 	return ret;
 }
 
+/*
+ * Appends a linked list chunk to the one in head (if it's not NULL)
+ * Returns the new pointer or NULL in case of failure.
+ * If the head is NULL, the added item is just created
+ */
+GNLChunk *chunk_add(GNLChunk *head) {
+GNLChunk *ret;
+	if (! (ret = (GNLChunk *) malloc(sizeof(GNLChunk)))) {
+		return NULL;
+	}
+	if (head)
+		head->next = ret;
+	return ret;
+}
+
+/*
+	If dest is NULL, simply releases the whole list.
+
+	If dest is not NULL, unwinds and copies buffers in the linked list,
+	for n bytes, into dest.
+	Every element that is copied is freed.
+	If there are more chunks after the element at n distance,
+	the first that is left is returned as the new head.
+
+
+	Returns:
+		a pointer to the new head, or NULL in case of failure.
+		always returns NULL if dest is passed as NULL
+*/
+GNLChunk *pack_and_free(char *dest, GNLChunk *head, ssize_t n) {
+GNLChunk *ret;
+ssize_t i;
+
+	i = 0;
+	while (dest && i < head->len) {
+		dest[i] = head->buf[i];
+		i++;
+	}
+	if (head->next) { // is there a follow up?
+		ret = (i < n || !dest) ? pack_and_free(dest ? &dest[i] : NULL, head->next, n - i) : head->next;
+	}
+	if (i >= n || !dest) // member fully consumed, or they want to free anyway
+		free(head);
+	return ret;
+}
+
 char *get_next_line(int fd) {
 static GNLTracker track;
 ssize_t r_b; // bytes read
 ssize_t lf_dist; // distance to the next LF in the linked list
 GNLChunk *read_tgt; // read target
 
-	expand_fd_catalog(&track, fd);
+	if (! expand_fd_catalog(&track, fd))
+		return NULL;
+
 	lf_dist = 0;
-	while ((read_tgt = (GNLChunk *) malloc(sizeof(GNLChunk)))) {
+	while ((read_tgt = chunk_add(NULL))) {
 		if ((r_b = read(fd, read_tgt->buf, BUFFER_SIZE)) <= 0) {
-			free(read_tgt);
-			break; // error or EOF. We'll handle that later
+			free(read_tgt); // useless allocation
+			break;
 		}
-		// to avoid copying memory, we always read directly into the chunk.
+		// to avoid copying memory twice, we always read directly into the chunk.
 		// If the list has no head, that is what read_tgt will be
 		if ((track.latest[fd] = track.latest[fd] ? track.latest[fd] : read_tgt) != read_tgt)
 			track.latest[fd]->next = read_tgt; // initiate or append to the list
-		while (read_tgt->mark++ < r_b) {
-			lf_dist++;
-			if (read_tgt->buf[read_tgt->mark] == '\n') {
-				printf("DIST_B: R=%lu M=%lu D=%lu S=`%s`\n", r_b, read_tgt->mark, lf_dist, read_tgt->buf);
+		while (read_tgt->len++ < r_b) {
+			if (read_tgt->buf[read_tgt->len] == '\n') {
+				if (read_tgt->buf[read_tgt->len] >= r_b)
+					read_tgt = chunk_add(read_tgt); // encountered newline, but there are more bytes
+				printf("DIST_B: R=%lu M=%lu D=%lu S=`%s`\n", r_b, read_tgt->len, lf_dist, read_tgt->buf);
 				break;
 			}
+			lf_dist++;
 		}
 	}
 
-	if (r_b >= 0)
-		;
+	if (r_b >= 0) {
+	}
 	
 	//free_chunks(track.latest[fd]);
 	return NULL;
