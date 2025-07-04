e14255773ea8a44c33cb7b12351e8e3340fb5b21
Agata Julia Zielnica <azielnic@c3r8p5.42vienna.com>
2025-07-04 19:13:31 +0200

    updated header

--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,28 @@
 /*   By: azielnic <azielnic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 18:11:48 by azielnic          #+#    #+#             */
-/*   Updated: 2025/06/13 16:59:44 by azielnic         ###   ########.fr       */
+/*   Updated: 2025/07/04 18:18:06 by azielnic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 # include <stdarg.h>
 # include <unistd.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
 
 char	*get_next_line(int fd);
+char	*ft_strdup(const char *s);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	*ft_calloc(size_t nmemb, size_t size);
+size_t	ft_strlen(const char *str);
 
 #endif
