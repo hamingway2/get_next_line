b0d6e6145fe6a540d1e8808f07645ee6d26a9f47
Agata Julia Zielnica <azielnic@c3r8p6.42vienna.com>
2025-07-06 00:35:10 +0200

    working version with one missing free

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: azielnic <azielnic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 15:53:43 by azielnic          #+#    #+#             */
-/*   Updated: 2025/07/05 18:27:04 by azielnic         ###   ########.fr       */
+/*   Updated: 2025/07/06 00:34:20 by azielnic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ static char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 // Function that splits at /n and returns up until incl that 
 // new line character. Updates the content of hold.
-static char	*extract_line(char **hold)
+static char	*extract_line(char **hold) //explain above better why double poiter is used
 {
 	char	*line;
 	char	*temp;
@@ -74,23 +74,26 @@ char	*get_next_line(int fd)
 	if (!hold)
 		hold = ft_calloc(1, 1);
 	read_count = 0;
-	while (!ft_strchr(hold, '\n') && ((read_count = read(fd, buffer, BUFFER_SIZE)) > 0))
+	while (!ft_strchr(hold, '\n'))
 	{
+		read_count = read(fd, buffer, BUFFER_SIZE);
+		if (read_count == -1)
+			return (free(hold), hold = NULL, NULL);
+		if (read_count == 0)
+			break;
 		buffer[read_count] = '\0';
 		temp = ft_strjoin(hold, buffer);
 		free(hold);
 		hold = temp;
 	}
 	free(buffer);
-	if (read_count == -1 || !hold || !*hold)
-		return (free(hold), hold = NULL, NULL);
 	return (extract_line(&hold));
 }
-/*
+
 int	main(void)
 {
 	char	*line;
-	int		fd = open("big_line_no_nl", O_RDONLY);
+	int		fd = open("text.txt", O_RDONLY);
 
 	if (fd < 0)
 		return (perror("open"), 1);
@@ -101,4 +104,4 @@ int	main(void)
 	}
 	close(fd);
 	return (0);
-}*/
+}
